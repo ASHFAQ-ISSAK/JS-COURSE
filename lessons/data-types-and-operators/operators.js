432bb97b94d38e87295dbc2b62a0e226ac9c0d69
ASHFAQ-ISSAK <ashfaq.abdi@gmail.com>
2024-07-20 19:06:07 +0300

    finished operators

--- lessons/data-types-and-operators/operators.js
+++ lessons/data-types-and-operators/operators.js
@@ -10,11 +10,37 @@
  */
 
 // 1. make variable and increment
+let age = 1 
+age+=1
+console.log (age)
 
 // 2. make 2 variables that are numbers and add them together
+let s = 6
+let n = 9
 
+const result = s+n
+console.log(result)
 // 3. make 3 variables that are numbers and multiply them together
-
+const multipliedNums =s * n * age
+console.log(multipliedNums)
 // 4. make 2 variables that are strings and concatenate them
+let shayIsHere = "Shay i see you "
+let satisfiedNow = "and I just broke the 4th wall"
 
+const msg = shayIsHere + satisfiedNow
+
+console.log(msg)
 // 5. make 2 variables that are numbers and compare them
+let ageOfAsh = 27;
+let ageOfMinnie = 40;
+
+// Compare the variables
+if (ageOfAsh > ageOfMinnie) {
+    console.log("Ash is older than Minnie.");
+} else if (ageOfAsh < ageOfMinnie) {
+    console.log("Ash is younger than Minnie.");
+} else {
+    console.log("Ash and Minnie are of the same age.");
+}
+
+
