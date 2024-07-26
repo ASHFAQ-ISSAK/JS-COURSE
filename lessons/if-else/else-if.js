0e1a8e4c583f8ba80c029d0350e388e6fe4c68c7
ASHFAQ-ISSAK <ashfaq.abdi@gmail.com>
2024-07-27 00:11:06 +0300

    done else if task

--- lessons/if-else/else-if.js
+++ lessons/if-else/else-if.js
@@ -7,3 +7,26 @@
 // if(_____) {}
 // else if(_____) {}
 // else {}
+let marksAnthony = ["Anthony", 54];
+let marksMoris = ["Morris", 95];
+let marksAsh = ["Ash", 34];
+
+const getGrade = (student) => {
+    if (student[1] >= 90) {
+        console.log(`${student[0]} got an A`);
+    } else if (student[1] >= 75) {
+        console.log(`${student[0]} got a B`);
+    } else if (student[1] >= 50) {
+        console.log(`${student[0]} got a C`);
+    } else if (student[1] >= 35) {
+        console.log(`${student[0]} got a D`);
+    } else {
+        console.log(`${student[0]} got an E`);
+    }
+};
+
+getGrade(marksAnthony);
+getGrade(marksMoris);
+getGrade(marksAsh);
+
+
